94f50dd4bcddf67295a5f126ddcecf85aa521082
Erwin <erwinjbofetiado@gmail.com>
2021-05-09 12:15:21 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
-import { allTimeFavorites } from "./Example.js";
+import { defaultToppings } from "./Example.js";
 import Toppings from "./Toppings";
 
 function App() {
+  const findToppings = (id) => {
+    //toppings is index of defaultToppings
+    return defaultToppings.find((topping) => {
+      return topping.id === id;
+    });
+  };
+
   return (
     <Switch>
       <Route exact path='/' render={() => <h1>Profile</h1>} />
-      <Route exact path='/toppings/:mbid' render={() => <h1>Toppings</h1>} />
+      <Route
+        exact
+        path='/toppings/:id'
+        render={(routeProps) => (
+          <Toppings
+            title={findToppings(routeProps.match.params.id).title}
+            albums={findToppings(routeProps.match.params.id).albums}
+          />
+        )}
+      />
     </Switch>
   );
-  /* <div className='App'>
-        {
-          <Toppings
-            title={allTimeFavorites.title}
-            albums={allTimeFavorites.albums}
-          />
-        }
-      </div> */
 }
 
 export default App;
