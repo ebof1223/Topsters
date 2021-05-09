82a150df1a94178aab06f1f710d9c9731a3e22df
Erwin <erwinjbofetiado@gmail.com>
2021-05-09 10:59:54 -0500

    added router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import { allTimeFavorites } from "./Example.js";
 import Toppings from "./Toppings";
 
 function App() {
   return (
-    <div className='App'>
-      {
-        <Toppings
-          title={allTimeFavorites.title}
-          albums={allTimeFavorites.albums}
-        />
-      }
-    </div>
+    <Switch>
+      <Route exact path='/' render={() => <h1>Profile</h1>} />
+      <Route exact path='/toppings/:mbid' render={() => <h1>Toppings</h1>} />
+    </Switch>
   );
+  /* <div className='App'>
+        {
+          <Toppings
+            title={allTimeFavorites.title}
+            albums={allTimeFavorites.albums}
+          />
+        }
+      </div> */
 }
 
 export default App;
