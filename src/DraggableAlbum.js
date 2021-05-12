d993bdff09e0adf4e97299e06a01fde8651b8766
Erwin <erwinjbofetiado@gmail.com>
2021-05-12 14:34:22 -0500

    added delete styling

--- src/DraggableAlbum.js
+++ src/DraggableAlbum.js
@@ -1,4 +1,5 @@
 import { withStyles } from "@material-ui/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const styles = {
   root: {
@@ -9,7 +10,34 @@ const styles = {
     position: "relative",
     cursor: "pointer",
     marginBottom: "-3.5px",
+    "&:hover svg": {
+      color: "white",
+      transition: "0.5s",
+      transform: "scale(1.2)",
+    },
   },
+  boxContent: {
+    position: "absolute",
+    width: "100%",
+    left: 0,
+    bottom: 0,
+    padding: " 10px",
+    color: "black",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "0.7rem",
+  },
+  links: {
+    position: "absolute",
+    display: "inline-block",
+    margin: "0 auto",
+    bottom: 0,
+    right: 0,
+    opacity: 1,
+    marginRight: "0.25rem",
+    marginBottom: "0.25rem",
+  },
+  deleteIcon: {},
 };
 function DraggableAlbum({ cover, classes }) {
   return (
@@ -18,7 +46,13 @@ function DraggableAlbum({ cover, classes }) {
         background: `url(${cover}) no-repeat center center/cover`,
       }}
       className={classes.root}
-    />
+    >
+      <div className={classes.boxContent}>
+        <span className={classes.links}>
+          <DeleteIcon className={classes.deleteIcon} />
+        </span>
+      </div>
+    </div>
   );
 }
 
