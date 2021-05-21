2e0558c94befc4df12678d5d1395fb2eaec0470b
Erwin <erwinjbofetiado@gmail.com>
2021-05-21 14:00:35 -0500

    implement token refresh

--- server/server.js
+++ server/server.js
@@ -6,24 +6,28 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-// app.post('/refresh', (req, res) => {
-//   const refreshToken = req.body.refreshToken;
-//   const spotifyApi = new SpotifyWebApi({
-//     redirectUri: 'http://localhost:3000',
-//     clientId: '0423b49839824b2d8056e7a5a8666622',
-//     clientSecret: '6eeb336d2e414f65bc0eba7367f4f58f',
-//     refreshToken,
-//   });
+app.post('/refresh', (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: '0423b49839824b2d8056e7a5a8666622',
+    clientSecret: '6eeb336d2e414f65bc0eba7367f4f58f',
+    refreshToken,
+  });
 
-//   spotifyApi
-//     .refreshAccessToken()
-//     .then((data) => {
-//       console.log(data.body);
-//     })
-//     .catch(() => {
-//       res.sendStatus(400);
-//     });
-// });
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expires: data.body.expiresIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
 
 app.post('/login', (req, res) => {
   const code = req.body.code;
@@ -43,8 +47,7 @@ app.post('/login', (req, res) => {
       });
       console.log(data);
     })
-    .catch((err) => {
-      console.log(err);
+    .catch(() => {
       res.sendStatus(400);
     });
 });
