177e7ed8baf14f594f2534eea8a70ff300adfeee
Erwin <erwinjbofetiado@gmail.com>
2021-05-22 21:16:56 -0500

    init grid change

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import Snackbar from '@material-ui/core/Snackbar';
-import IconButton from '@material-ui/core/IconButton';
-import DoneIcon from '@material-ui/icons/Done';
+import Button from '@material-ui/core/Button';
 import styles from './styles/NavbarStyles';
 
 function Navbar({
@@ -22,28 +18,18 @@ function Navbar({
           Toppings
         </Link>
       </div>
-      <div className={classes.selectContainer}>
-        <Select onChange={handleMusicProviderChange} value={musicProvider}>
-          <MenuItem value="spotify">Spotify</MenuItem>
-          <MenuItem value="applemusic">Apple Music</MenuItem>
-          <MenuItem value="youtube">YouTube</MenuItem>
-        </Select>
-      </div>
-      <Snackbar
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-        open={open}
-        autoHideDuration={2000}
-        message={
-          <span id="message-id">Music Provider Successfully Changed!</span>
-        }
-        onClose={() => setOpen(false)}
-        ContentProps={{ 'aria-describedby': 'message-id' }}
-        action={[
-          <IconButton color="inherit" key="done" aria-label="done">
-            <DoneIcon />
-          </IconButton>,
-        ]}
-      ></Snackbar>
+      <Link to={'/'} style={{ textDecoration: 'none', marginLeft: '2rem' }}>
+        <Button variant="contained" color="secondary" type="submit">
+          Go Back
+        </Button>
+      </Link>
+      <Button
+        variant="contained"
+        color="primary"
+        // onClick={handleClickOpen}
+      >
+        Edit
+      </Button>
     </header>
   );
 }
